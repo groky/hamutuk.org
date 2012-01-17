470918ae8bd01cd24cb22405499a4f3815b0c78d
Kyle Grovers <kyle.grovers@gmail.com>
2012-01-17 17:05:57 +1030

    Created the initial framework for Hamutuk.org

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HamutukOrg::Application.config.secret_token = '853ea1215dcc07f2f6488ff404c82b856afcf2f6bef31b0495aefd85ecfa115e1541639da4d07f15f7c4a97191e40dfbfb5a97578393306c4acb00ed372f3f98'
